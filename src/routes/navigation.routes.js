b4548fd03e713a5c548a8f7971d86a9541cda709
Lucyan Ovídio <lucyanovidio@gmail.com>
2022-11-11 01:27:56 -0300

    fix: Correção na view principal index

--- src/routes/navigation.routes.js
+++ src/routes/navigation.routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render("home_view");
+    res.render("index");
 });
   
 router.get('/about', (req, res) => {
